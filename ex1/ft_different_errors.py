4f3eea9b837b7db7167d5ad1981d31a4117a550d
Logann Leriche <lleriche@c2r12p6.42lehavre.fr>
2026-01-27 17:36:09 +0100

    module2

--- ex1/ft_different_errors.py
+++ ex1/ft_different_errors.py
@@ -2,6 +2,7 @@ def garden_operations(temp_str):
     try:
         temp = int(temp_str)
         temp = 5 / temp
+        temp = open(temp_str)
     except ValueError:
         print("Caught ValueError: invalid literal for int()")
     except ZeroDivisionError:
@@ -10,4 +11,19 @@ def garden_operations(temp_str):
         print("Caught FileNotFoundError: No such file 'missing.txt'")
     except KeyError:
         print("Caught KeyError: 'missing\_plant'")
-    
+
+
+def test_error_types():
+    print("=== Garden Error Types Demo ===")
+    print()
+    print("Testing ValueError...")
+    garden_operations("abc")
+    print()
+    print("Testing ZeroDivisionError...")
+    garden_operations(0)
+    print()
+    print("Testing ZeroDivisionError...")
+    garden_operations("")
+
+
+test_error_types()
